4ef23ac7ee5f4872ef1b7067c1d1cc420366954c
94ivana <todoroska65@gmail.com>
2023-02-07 16:26:29 +0100

    End of section 18

--- API/Interfaces/IMessageRepository.cs
+++ API/Interfaces/IMessageRepository.cs
@@ -17,8 +17,6 @@ namespace API.Interfaces
 
         Task<IEnumerable<MessageDto>> GetMessageThread(string currentUserNam, string recipientUserName);
 
-        Task<bool> SaveAllAsync();
-
         void AddGroup(Group group);
 
         void RemoveConnection(Connection connection);
